69364c3e20fb0eb24be756696317a3277d78991d
sherryzia <shaheerzia22@gmail.com>
2026-03-15 15:40:56 +0500

    added json files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/.well-known/apple-app-site-association',
+        headers: [
+          { key: 'Content-Type', value: 'application/json' },
+        ],
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
